eb16e9f38f83d04e002b642f16b5282179cdd7f4
Aikar <aikar@aikar.co>
2016-04-03 17:48:50 -0400

    Fix Cancelling BlockPlaceEvent triggering physics

--- src/main/java/net/minecraft/server/level/ServerLevel.java
+++ src/main/java/net/minecraft/server/level/ServerLevel.java
@@ -1458,6 +1458,7 @@ public class ServerLevel extends Level implements WorldGenLevel {
 
     @Override
     public void updateNeighborsAt(BlockPos pos, Block sourceBlock) {
+        if (captureBlockStates) { return; } // Paper - Cancel all physics during placement
         this.neighborUpdater.updateNeighborsAtExceptFromFacing(pos, sourceBlock, (Direction) null);
     }
 
