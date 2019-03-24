d2f1e43400fdafe22a012218c2f97b4e7397f809
Aikar <aikar@aikar.co>
2019-03-24 00:24:52 -0400

    Entity#getEntitySpawnReason
    Allows you to return the SpawnReason for why an Entity Spawned
    
    Pre existing entities will return NATURAL if it was a non
    persistenting Living Entity, SPAWNER for spawners,
    or DEFAULT since data was not stored.

--- src/main/java/net/minecraft/server/level/ServerLevel.java
+++ src/main/java/net/minecraft/server/level/ServerLevel.java
@@ -1289,6 +1289,7 @@ public class ServerLevel extends Level implements WorldGenLevel {
             return true;
         }
         // Paper end
+        if (entity.spawnReason == null) entity.spawnReason = spawnReason; // Paper
         if (entity.isRemoved()) {
             // Paper start
             if (DEBUG_ENTITIES) {
