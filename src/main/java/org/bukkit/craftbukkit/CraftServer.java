cc8d733ea3d16beaa2a9a44ad5f1aa39a2a7a4ca
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2020-05-01 17:39:26 +0300

    Expose game version

--- src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -593,6 +593,13 @@ public final class CraftServer implements Server {
         return this.bukkitVersion;
     }
 
+    // Paper start - expose game version
+    @Override
+    public String getMinecraftVersion() {
+        return console.getServerVersion();
+    }
+    // Paper end
+
     @Override
     public List<CraftPlayer> getOnlinePlayers() {
         return this.playerView;
