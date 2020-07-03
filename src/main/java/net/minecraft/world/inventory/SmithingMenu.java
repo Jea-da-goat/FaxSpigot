36ad2fcdb571193d7d53804551d7ba4f4f45ae71
William Blake Galbreath <Blake.Galbreath@GMail.com>
2020-07-03 11:58:56 -0500

    Add PrepareResultEvent
    Adds a new event for all crafting stations that generate a result slot item
    
    Anvil, Grindstone and Smithing now extend this event

--- src/main/java/net/minecraft/world/inventory/SmithingMenu.java
+++ src/main/java/net/minecraft/world/inventory/SmithingMenu.java
@@ -79,6 +79,7 @@ public class SmithingMenu extends ItemCombinerMenu {
             }
         }
 
+        org.bukkit.craftbukkit.event.CraftEventFactory.callPrepareResultEvent(this, RESULT_SLOT); // Paper
     }
 
     @Override
