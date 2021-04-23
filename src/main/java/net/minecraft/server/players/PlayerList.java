ba28a37b7a6522fbe125e513ee0eecb9a6e5dfd8
HexedHero <6012891+HexedHero@users.noreply.github.com>
2021-04-23 22:42:42 +0100

    Fix anchor respawn acting as a bed respawn from the end portal

--- src/main/java/net/minecraft/server/players/PlayerList.java
+++ src/main/java/net/minecraft/server/players/PlayerList.java
@@ -789,6 +789,7 @@ public abstract class PlayerList {
 
         // Paper start
         boolean isBedSpawn = false;
+        boolean isAnchorSpawn = false;
         boolean isRespawn = false;
         boolean isLocAltered = false; // Paper - Fix SPIGOT-5989
         // Paper end
@@ -809,6 +810,7 @@ public abstract class PlayerList {
                 if (optional.isPresent()) {
                     BlockState iblockdata = worldserver1.getBlockState(blockposition);
                     boolean flag3 = iblockdata.is(Blocks.RESPAWN_ANCHOR);
+                    isAnchorSpawn = flag3; // Paper - Fix anchor respawn acting as a bed respawn from the end portal
                     Vec3 vec3d = (Vec3) optional.get();
                     float f1;
 
@@ -837,7 +839,7 @@ public abstract class PlayerList {
             }
 
             Player respawnPlayer = entityplayer1.getBukkitEntity();
-            PlayerRespawnEvent respawnEvent = new PlayerRespawnEvent(respawnPlayer, location, isBedSpawn && !flag2, flag2);
+            PlayerRespawnEvent respawnEvent = new PlayerRespawnEvent(respawnPlayer, location, isBedSpawn && !isAnchorSpawn, isAnchorSpawn); // Paper - Fix anchor respawn acting as a bed respawn from the end portal
             this.cserver.getPluginManager().callEvent(respawnEvent);
             // Spigot Start
             if (entityplayer.connection.isDisconnected()) {
