Akarin-project/StarLink

Allow Reading Old Large Chunks
The size of chunks in the region format is overdetermined. In particular their size on disk is indicated by both a sector count in the header, and actual size in the body.
If their size would overflow the header field (>= 255 sectors), it can just be read directly from the body instead.

This code/concept was adapted from MinecraftForge.

src/main/java/net/minecraft/server/RegionFile.java
@@ -65,6 +65,14 @@ public class RegionFile implements AutoCloseable {
                     if (k != 0) {
                         int l = b(k);
                         int i1 = a(k);
+                        // Spigot start
+                        if (i1 == 255) {
+                            // We're maxed out, so we need to read the proper length from the section
+                            ByteBuffer realLen = ByteBuffer.allocate(4);
+                            this.dataFile.read(realLen, l * 4096);
+                            i1 = (realLen.getInt(0) + 4) / 4096 + 1;
+                        }
+                        // Spigot end
 
                         this.freeSectors.a(l, i1);
                     }
@@ -89,6 +97,13 @@ public class RegionFile implements AutoCloseable {
         } else {
             int j = b(i);
             int k = a(i);
+            // Spigot start
+            if (k == 255) {
+                ByteBuffer realLen = ByteBuffer.allocate(4);
+                this.dataFile.read(realLen, j * 4096);
+                k = (realLen.getInt(0) + 4) / 4096 + 1;
+            }
+            // Spigot end
             int l = k * 4096;
             ByteBuffer bytebuffer = ByteBuffer.allocate(l);
 

Jea-da-goat/FaxSpigot

src/main/java/net/minecraft/world/level/chunk/storage/RegionFile.java
@@ -84,6 +84,14 @@ public class RegionFile implements AutoCloseable {
                     if (l != 0) {
                         int i1 = getSectorNumber(l);
                         int j1 = getNumSectors(l);
+                        // Spigot start
+                        if (j1 == 255) {
+                            // We're maxed out, so we need to read the proper length from the section
+                            ByteBuffer realLen = ByteBuffer.allocate(4);
+                            this.file.read(realLen, i1 * 4096);
+                            j1 = (realLen.getInt(0) + 4) / 4096 + 1;
+                        }
+                        // Spigot end
 
                         if (i1 < 2) {
                             RegionFile.LOGGER.warn("Region file {} has invalid sector at index: {}; sector {} overlaps with header", new Object[]{path, k, i1});
@@ -119,6 +127,13 @@ public class RegionFile implements AutoCloseable {
         } else {
             int j = getSectorNumber(i);
             int k = getNumSectors(i);
+            // Spigot start
+            if (k == 255) {
+                ByteBuffer realLen = ByteBuffer.allocate(4);
+                this.file.read(realLen, j * 4096);
+                k = (realLen.getInt(0) + 4) / 4096 + 1;
+            }
+            // Spigot end
             int l = k * 4096;
             ByteBuffer bytebuffer = ByteBuffer.allocate(l);
 