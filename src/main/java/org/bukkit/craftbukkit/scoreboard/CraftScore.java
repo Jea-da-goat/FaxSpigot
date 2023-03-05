PaperMC/Paper

Add API for resetting a single score
It was only possible to reset all scores for a specific entry, instead of resetting only specific scores.

paper-server/src/main/java/org/bukkit/craftbukkit/scoreboard/CraftScore.java
@@ -66,4 +66,12 @@ final class CraftScore implements Score {
     public CraftScoreboard getScoreboard() {
         return this.objective.getScoreboard();
     }
+
+    // Paper start
+    @Override
+    public void resetScore() {
+        Scoreboard board = this.objective.checkState().board;
+        board.resetSinglePlayerScore(entry, this.objective.getHandle());
+    }
+    // Paper end
 }

Jea-da-goat/FaxSpigot

src/main/java/org/bukkit/craftbukkit/scoreboard/CraftScore.java
@@ -68,4 +68,12 @@ final class CraftScore implements Score {
     public CraftScoreboard getScoreboard() {
         return this.objective.getScoreboard();
     }
+
+    // Paper start
+    @Override
+    public void resetScore() {
+        Scoreboard board = this.objective.checkState().board;
+        board.resetPlayerScore(entry, this.objective.getHandle());
+    }
+    // Paper end
 }