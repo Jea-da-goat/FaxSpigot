pvginkel/Tweakkit-Server

BungeeCord Support
Provides support for IP forwarding via BungeeCord.

src/main/java/net/minecraft/server/NetworkManager.java
@@ -36,7 +36,8 @@ public class NetworkManager extends SimpleChannelInboundHandler {
     private final Queue k = Queues.newConcurrentLinkedQueue();
     private final Queue l = Queues.newConcurrentLinkedQueue();
     private Channel m;
-    private SocketAddress n;
+    public SocketAddress n; // Spigot
+    public String spoofedUUID; // Spigot
     private PacketListener o;
     private EnumProtocol p;
     private IChatBaseComponent q;
@@ -196,4 +197,11 @@ public class NetworkManager extends SimpleChannelInboundHandler {
     static Channel a(NetworkManager networkmanager) {
         return networkmanager.m;
     }
+
+    // Spigot Start
+    public SocketAddress getRawAddress()
+    {
+        return this.m.remoteAddress();
+    }
+    // Spigot End
 }

src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1289,6 +1289,12 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     {
 
         @Override
+        public InetSocketAddress getRawAddress()
+        {
+            return (InetSocketAddress) getHandle().playerConnection.networkManager.getRawAddress();
+        }
+
+        @Override
         public boolean getCollidesWithEntities()
         {
             return getHandle().collidesWithEntities;

Jea-da-goat/FaxSpigot

src/main/java/net/minecraft/network/NetworkManager.java
@@ -74,6 +74,10 @@ public class NetworkManager extends SimpleChannelInboundHandler<Packet<?>> {
     private final Queue<NetworkManager.QueuedPacket> queue = Queues.newConcurrentLinkedQueue();
     public Channel channel;
     public SocketAddress address;
+    // Spigot Start
+    public java.util.UUID spoofedUUID;
+    public com.mojang.authlib.properties.Property[] spoofedProfile;
+    // Spigot End
     private PacketListener packetListener;
     private IChatBaseComponent disconnectedReason;
     private boolean encrypted;
@@ -437,4 +441,11 @@ public class NetworkManager extends SimpleChannelInboundHandler<Packet<?>> {
             this.listener = packetsendlistener;
         }
     }
+
+    // Spigot Start
+    public SocketAddress getRawAddress()
+    {
+        return this.channel.remoteAddress();
+    }
+    // Spigot End
 }

src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1968,6 +1968,12 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     {
 
         @Override
+        public InetSocketAddress getRawAddress()
+        {
+            return (InetSocketAddress) getHandle().connection.connection.getRawAddress();
+        }
+
+        @Override
         public boolean getCollidesWithEntities() {
             return CraftPlayer.this.isCollidable();
         }